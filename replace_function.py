45d7fe255cd2007ae0c2059e3d5f8cb4c4983918
JacobMason83 <jmason062083@gmail.com>
2020-11-13 12:21:00 -0500

    replace function

---
+++ replace_function.py
@@ -0,0 +1,7 @@
+sentence = 'The quick brown fox jumped over the lazy dog'
+
+# sentence = 'New Value' normal way to do it
+
+sentence = sentence.replace('quick', 'slow')    #replaces quick with slow
+
+print(sentence)
