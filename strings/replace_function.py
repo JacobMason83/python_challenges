6a60b08a93b9f65a8e1e78e098805a7592d71256
JacobMason83 <jmason062083@gmail.com>
2020-11-13 12:25:16 -0500

    added notes to it

--- strings/replace_function.py
+++ strings/replace_function.py
@@ -2,6 +2,6 @@ sentence = 'The quick brown fox jumped over the lazy dog'
 
 # sentence = 'New Value' normal way to do it
 
-sentence = sentence.replace('quick', 'slow')    #replaces quick with slow
+sentence = sentence.replace('quick', 'slow')    #replaces quick with slow makes it better for immutabiltiy 
 
 print(sentence)
