8629f17929af1e1af9d841458a9bf29b47fe2c9c
JacobMason83 <jmason062083@gmail.com>
2020-11-16 18:28:27 -0500

    added a high level overview of dictionaries

---
+++ strings/dictionaries.py
@@ -0,0 +1,14 @@
+players = {
+    'ss': 'Correa',
+    '2b': 'Altuve',
+    '3b': 'Bragmen',
+    'DH': 'Gattis',
+    'OF': 'Springer'
+}
+ #python is very good at giving errors for data issues rather than other languages who will print null
+second_base = players['2b']
+designated_hitter = players['DF']
+out_fielder = players['OF']
+print(players)
+print(designated_hitter)
+print(out_fielder)
