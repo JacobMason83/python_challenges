47f45f6eddbc956740a586546de238566b12bca3
JacobMason83 <jmason062083@gmail.com>
2020-11-17 12:55:11 -0500

    added nested dictionaries and how to add key:value pairs to the dictionary

--- strings/dictionaries.py
+++ strings/dictionaries.py
@@ -7,8 +7,25 @@ players = {
 }
  #python is very good at giving errors for data issues rather than other languages who will print null
 second_base = players['2b']
-designated_hitter = players['DF']
+designated_hitter = players['DH']
 out_fielder = players['OF']
 print(players)
 print(designated_hitter)
 print(out_fielder)
+#nested collections within a dictionary 
+teams = {
+     "astros": ["Altuve", "Correa", "Bregman"],
+     "angels": ["Trout", "Pujols"],
+     "yankees": ["Judge", "Stanton"]
+ }
+ #its all the same data as lists and you can work on it 
+# astros = teams['astros']
+# angels = teams['angels']
+# yankees = teams['yankees']
+# print(astros)
+# print(angels)
+# print(yankees)
+# how to add new key:value pairs to dictionaries 
+
+teams['red sox'] = ['Price', 'Betts']
+print(teams)
